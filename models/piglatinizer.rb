aa8dcfb8fc5bd743d0132762c52016d053869de5
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 23:09:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,18 @@ class PigLatinizer
     string_array = string.split(" ")
     piglatin_array = []
     index = 0
-    
+    string_array.each do |word|
+      word_array = word.split("")
+      if word_array[0].match(/[aeiouAEIOU]/)
+        word = word_array.join
+        word << "way"
+      else 
+        while word_array[index].match(/[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/)
+          
+          index += 1 
+        end 
+        word_array
+      end 
     end 
       
   end
