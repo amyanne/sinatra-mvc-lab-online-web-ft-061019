2a890fde4a4df24f433a98effded60da3a547a8e
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 23:12:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,6 @@ class App < Sinatra::Base
    end
    
   post '/piglatinize' do
-      "#{PigLatinizer.new.turn_string_piglatin(params[:user_phrase])}"
+      "#{PigLatinizer.new.piglatinize(params[:user_phrase])}"
    end
 end
